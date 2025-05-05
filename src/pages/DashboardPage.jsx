753151e459eea9c7343f5a721c18e9d44368c759
Yiannis <ithrive360.health@gmail.com>
2025-05-05 14:55:29 +0100

    Auto commit from deploy script

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,6 +1,15 @@
-import logo from '../assets/logo.png';
+import { supabase } from '../supabaseClient'
+import logo from '../assets/logo.png'
+import '../App.css'
 
 function DashboardPage({ user }) {
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.error('Logout error:', error.message)
+    }
+  }
+
   return (
     <div className="dashboard">
       <img src={logo} alt="iThrive360 Logo" className="logo" />
@@ -8,9 +17,12 @@ function DashboardPage({ user }) {
       <p>
         Welcome, {user.user_metadata?.name?.split(' ')[0] || user.email || 'there'}!
       </p>
-      <h1>SCREW YOU BOLTON 1 !!!</h1>
+      <h1>SCREW YOU BOLTON !!!</h1>
+      <button onClick={handleLogout} className="btn btn-primary">
+        Sign out
+      </button>
     </div>
-  );
+  )
 }
 
-export default DashboardPage;
+export default DashboardPage
