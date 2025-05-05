bd72c45131f58af9f84cc669ea46d677e6b00589
Yiannis <ithrive360.health@gmail.com>
2025-05-05 20:53:07 +0100

    Auto commit from deploy script

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -21,9 +21,11 @@ function AuthPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo
+        redirectTo,
+        skipBrowserRedirect: true
       }
     });
+  
     if (error) setMessage(error.message);
   };
   
