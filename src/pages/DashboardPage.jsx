fb7f3aeba87556593001beb68e8ce9e36300e84a
Yiannis <ithrive360.health@gmail.com>
2025-05-14 16:22:03 +0100

    Auto commit from deploy script

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -453,16 +453,15 @@ function DashboardPage() {
 
       <div style={{ height: 60 }} />
 
-      <div style={{ position: 'relative', textAlign: 'center', marginTop: 12 }}>
-        <h3 style={{ margin: 0, fontWeight: 600, display: 'inline-block', position: 'relative', paddingLeft: 48 }}>
-          {timeAnimation && (
-            <span style={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)', width: 64, height: 64 }}>
-              <Lottie animationData={timeAnimation} loop autoplay />
-            </span>
-          )}
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: 12 }}>
+        <div style={{ width: 32, height: 32, marginRight: 12 }}>
+          <Lottie animationData={timeAnimation} loop autoplay />
+        </div>
+        <h3 style={{ margin: 0, fontWeight: 600 }}>
           {greeting}, {user?.user_metadata?.full_name?.split(' ')[0] || user?.email || 'there'}
         </h3>
       </div>
+
 
 
       <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap', justifyContent: 'center', marginTop: '2rem' }}>
