ef79cb2c1f8f81c88e174002b73307a0b7d0761c
Yiannis <ithrive360.health@gmail.com>
2025-05-17 20:19:35 +0100

    Auto commit from deploy script

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -562,10 +562,9 @@ function DashboardPage() {
         profile={profile}
       />
 
-      <div style={{ height: 60 }} />
+      <div style={{ height: 20 }} />
       
-      
-      <div style={{ position: 'relative', textAlign: 'center', marginTop: 32 }}>
+      <div style={{ position: 'relative', textAlign: 'center', marginTop: 10 }}>
         <h3 style={{ display: 'inline-block', fontWeight: 600, margin: 0, position: 'relative' }}>
           {greeting}, {user?.user_metadata?.full_name?.split(' ')[0] || user?.email || 'there'}
           <span style={{
